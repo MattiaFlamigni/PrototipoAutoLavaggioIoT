2eb4681113eeb9c302dbd02d4506025c942ee7a1
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-15 12:54:47 +0100

    created sonar components

---
+++ src/components/Proximity.h
@@ -0,0 +1,12 @@
+#ifndef __PROXIMITY__
+#define __PROXIMITY__
+
+class Proximity {
+
+public:
+  virtual float getDistance() = 0;
+  
+};
+
+
+#endif
---
+++ src/components/Sonar.h
@@ -0,0 +1,24 @@
+#ifndef __SONAR__
+#define __SONAR__
+
+#include "Proximity.h"
+
+#define NO_OBJ_DETECTED -1
+
+class Sonar: public Proximity {
+
+public:  
+  Sonar(int echoPin, int trigPin, long maxTime);
+  float getDistance();
+  void setTemperature(float temp);  
+
+private:
+    const float vs = 331.5 + 0.6*20;
+    float getSoundSpeed();
+    
+    float temperature;    
+    int echoPin, trigPin;
+    long timeOut;
+};
+
+#endif 
