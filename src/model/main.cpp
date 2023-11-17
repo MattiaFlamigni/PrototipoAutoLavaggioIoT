6dd574c538309f8be995837685bd95735edbbf76
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-17 18:54:56 +0100

    tested temp task

--- src/model/main.cpp
+++ src/model/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h> 
+#include "task/Task.h"
+#include "task/TemperatureControlTask.h"
+#include "task/Scheduler.h"
 
-
-
+Scheduler sched;
 void setup() {
+    Serial.begin(9600);
+    sched.init(100);
+    Task* temperature = new TemperatureControlTask();
+    temperature->init(50);
+
+    sched.addTask(temperature);
 }
 
 void loop() {
+    sched.schedule();
 }
