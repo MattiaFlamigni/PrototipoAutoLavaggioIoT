791573cef820a071e9fb94f94a38579108c83210
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-19 14:27:24 +0100

    semplified button logic

--- src/components/ButtonImpl.cpp
+++ src/components/ButtonImpl.cpp
@@ -3,31 +3,10 @@
 
 ButtonImpl::ButtonImpl(int pin){
   this->pin = pin;
-  pinMode(pin, INPUT);  
-  sync();   
+  pinMode(pin, INPUT); 
 } 
   
 bool ButtonImpl::isPressed(){
-  return pressed;
+  return digitalRead(pin)==HIGH;
 }
 
-bool ButtonImpl::isClicked(){
-  return clicked;
-}
-
-void ButtonImpl::sync(){
-  bool wasPressed = pressed;
-  pressed = digitalRead(pin) == HIGH;
-  if (!pressed){
-    if (wasPressed){
-      clicked = true;
-    } else {
-      if (clicked){
-        clicked = false;
-      }
-    }
-  } else if (pressed){
-    clicked = false;
-  }
-  updateSyncTime(millis());
-}
