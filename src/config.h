8fbf993bb9e6dbca1e2202dfbd2fbcf1b944806c
FilippoBadioli <filippo.badioli@studio.unibo.it>
2023-11-20 13:19:12 +0100

    updated config file

--- src/config.h
+++ src/config.h
@@ -1,11 +1,21 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define BUTTON_MAINTENACE_PIN 7
+//Pins
+#define START_BUTTON_PIN 7
 #define TMP_SENSOR_PIN A0
 #define GREEN_LED_1 3
 #define GREEN_LED_2 5
 #define RED_LED 4
+#define ECHO_PIN 100 //TODO
+#define TRIG_PIN 100 //TODO
+#define SONAR_TIME 400 //TODO
+#define PIR_PIN 100 //TODO
+#define SERVO_PIN 100 //TODO
+
+//Constants
+#define MINDIST 100 //TODO
+#define MAXDIST 600 //TODO
 
 
 
