ac71fd36e5f53ad3bd200db4b3f93c3188ce5b60
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-21 14:59:24 +0100

    Implemented lcd component

--- src/components/Lcd.cpp
+++ src/components/Lcd.cpp
@@ -1,11 +1,20 @@
 #include "Lcd.h"
 #include "Arduino.h"
 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4); 
+
+
 Lcd::Lcd(int sda, int scl) {
     this->sda = sda;
     this->scl = scl;
+    lcd.begin(20,4);
+    lcd.backlight();
+    pinMode(9,OUTPUT);
+    digitalWrite(9,50);
+    
 }
 
 void Lcd::display(char * msg) {
-    //TODO
+    lcd.print(msg);
 }
