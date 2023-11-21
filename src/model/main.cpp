fd6a7456dfacecbdba2d9623430a0c03e01f9283
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-21 13:31:37 +0100

    created car t ask

--- src/model/main.cpp
+++ src/model/main.cpp
@@ -2,15 +2,18 @@
 #include "task/Task.h"
 #include "task/TemperatureControlTask.h"
 #include "task/Scheduler.h"
+#include "task/CarPresenceTask.h"
 
 Scheduler sched;
 void setup() {
     Serial.begin(9600);
     sched.init(100);
     Task* temperature = new TemperatureControlTask();
+    Task* carControl = new CarPresenceTask();
     temperature->init(50);
 
     sched.addTask(temperature);
+    sched.addTask(carControl);
 }
 
 void loop() {
