05d39e100fb024876bcd4c27f541a39145b54741
mattiaflamigni4@studio.unibo.it <mattia.flamigni4@studio.unibo.it>
2023-11-22 10:29:56 +0100

    revisited time

--- src/model/main.cpp
+++ src/model/main.cpp
@@ -7,13 +7,13 @@
 Scheduler sched;
 void setup() {
     Serial.begin(9600);
-    sched.init(100);
+    sched.init(300);
     Task* temperature = new TemperatureControlTask();
     Task* carControl = new CarPresenceTask();
-    temperature->init(50);
-    carControl->init(100);
+    temperature->init(100);
+    carControl->init(30);
 
-    //sched.addTask(temperature);
+    sched.addTask(temperature);
     sched.addTask(carControl);
 }
 
